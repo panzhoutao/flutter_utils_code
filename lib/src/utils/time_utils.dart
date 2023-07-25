6ef0d74828240c8844eb7679fc5cecc0bc1aca1b
mrpan <836618033@qq.com>
2023-07-25 10:55:56 +0800

    1、add isSameDay function to TimeUtils;

--- lib/src/utils/time_utils.dart
+++ lib/src/utils/time_utils.dart
@@ -58,4 +58,11 @@ class TimeUtils {
     return '';
   }
 
+  /// 是否是同一天
+  static bool isSameDay(DateTime date1, DateTime date2) {
+    return date1.year == date2.year &&
+        date1.month == date2.month &&
+        date1.day == date2.day;
+  }
+
 }
